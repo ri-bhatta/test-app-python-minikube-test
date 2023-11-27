02c94bff8f8fe9cc7b27a1587a1e9b1987b98aad
ri-bhatta <57339760+ri-bhatta@users.noreply.github.com>
2023-11-27 18:35:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,8 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/healthz':
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(b'OK')
-        else:
-            self.send_response(404)
-            self.end_headers()
-def run(server_class=HTTPServer, handler_class=RequestHandler, port=80):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f'Starting server on port {port}...')
-    httpd.serve_forever()
-    print("completed")
+counter = 0
 
-if __name__ == '__main__':
-    run()
+while True:
+    print(f"Loop iteration: {counter}")
+    counter += 1
+
+    if counter >= 10:
+        break  # Break out of the loop when counter is greater than or equal to 10
